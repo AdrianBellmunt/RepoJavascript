5709acdeba24da5ccd8060ba0770bb8544c8e8f5
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-10-03 12:40:35 +0200

    03-10-22

--- Tema1/EjerciciosAvanzados2/Ejercicio2/Ejercicio2.js
+++ Tema1/EjerciciosAvanzados2/Ejercicio2/Ejercicio2.js
@@ -1,12 +1,14 @@
 function aviso() {
-    let envio = document.getElementById("aviso");
-    let dni = document.getElementById("texto");
+    let envio = document.getElementById("envio");
+    let dni = document.getElementById("dni");
     envio.addEventListener("click", (event) => {
         event.preventDefault()
         if (dni.value === "") {
             alert("esta en blanco");
+            
         } else {
             alert('todo ok');
+            
         }
       })
     
---
+++ Tema1/EjerciciosAvanzados2/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,12 @@
+function inicializar(){
+
+    valor.value="0";
+
+
+}
+
+function suma() {
+   
+valor.value = parseInt(valor.value) + 10;
+    
+}
