1e40886f9c6838825d8d1218ad738505e56c6247
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-10-10 12:07:34 +0200

    10-10-22

--- Tema1/DOM/Ejercicio2/E2.js
+++ Tema1/DOM/Ejercicio2/E2.js
@@ -1,33 +1,39 @@
 function anadir_parrafo(){
 
-    var parrafo = document.createElement('p');
-    var texto = document.createTextNode('Párrafo añadido.');
+    var parrafo = document.createElement('li');
+    var texto = document.createTextNode('tiburon.');
     parrafo.appendChild(texto);
-    document.getElementById('ej-aCh').appendChild(parrafo);
+    document.getElementById('animal').appendChild(parrafo);
     }
 
 function insertar_parrafo() {
 
-    var parrafo = document.createElement('p');
-    var texto = document.createTextNode('Párrafo insertado');
+    var parrafo = document.createElement('li');
+    var texto = document.createTextNode('gorila');
     parrafo.appendChild(texto);
-    document.getElementById('ej-aCh').appendChild(parrafo);
+    document.getElementById('animal').appendChild(parrafo);
 }
 
 function reemplazar(){
-    var nuevoParrafo = document.createElement('p');
-    nuevoParrafo.appendChild(document.createTextNode('parrafo remplazado'));
-    var viejoParrafo = document.getElementById('ej-aCh').getElementsByTagName('p')[1];
-    document.getElementById('ej-aCh').replaceChild(nuevoParrafo,viejoParrafo);
+    var nuevoParrafo = document.createElement('li');
+    nuevoParrafo.appendChild(document.createTextNode('animal remplazado'));
+    var viejoParrafo = document.getElementById('animal').getElementsByTagName('li')[1];
+    document.getElementById('animal').replaceChild(nuevoParrafo,viejoParrafo);
 }    
 
 function eliminar(){
-    var parrafoAEliminar = document.getElementById('ej-aCh').getElementsByTagName('p')[1];
-    document.getElementById('ej-aCh').removeChild(parrafoAEliminar);
+    var parrafoAEliminar = document.getElementById('animal').getElementsByTagName('li')[1];
+    document.getElementById('animal').removeChild(parrafoAEliminar);
 }
 
 function clonar(){
-    var clon = document.getElementById('ej-aCh').cloneNode(true);
-    document.getElementById('ej-aCh').parentNode.insertBefore(clon,document.getElementById('ej-aCh'));
+    var clon = document.getElementById('animal').cloneNode(true);
+    document.getElementById('animal').parentNode.insertBefore(clon,document.getElementById('animal'));
 }
     
+function SublistaDOM() {
+    var nuevoParrafo = document.createElement('ul');
+    nuevoParrafo.appendChild(document.createTextNode('Salvaje'));
+    var viejoParrafo = document.getElementById('animal').getElementsByTagName('ul')[1];
+    document.getElementById('animal').appendChild(viejoParrafo);
+}
