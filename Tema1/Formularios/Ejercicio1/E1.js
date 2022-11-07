245a5e52a46765962f8c08c2e8c4e47c13bded8b
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-11-07 12:09:20 +0100

    Validacion formulario

--- Tema1/Formularios/Ejercicio1/E1.js
+++ Tema1/Formularios/Ejercicio1/E1.js
@@ -1,14 +1,64 @@
 window.onload = function () {
     document.formulario.nombre.focus();
+
+    var x = document.getElementById("aficion");
+    x.addEventListener("change", aficion);
+
+    document.getElementById("form").addEventListener('submit', validarFormulario);
+}
+
+   function aficion() {
+     alert("La longitud es :"+document.formulario.aficion.length);
+     alert("El index escogido es :"+document.formulario.aficion.selectedIndex);
+     alert("La aficion escogida es :"+document.formulario.aficion.value);
+     
+   }
+
+    
+    contenido_textarea = ""
+    num_caracteres_permitidos = 150
+    
+    function valida_longitud(){
+       num_caracteres = document.forms[0].texto.value.length
+    
+       if (num_caracteres > num_caracteres_permitidos){
+          document.forms[0].texto.value = contenido_textarea
+       }else{
+          contenido_textarea = document.forms[0].texto.value
+       }
+    
+       if (num_caracteres >= num_caracteres_permitidos){
+          document.forms[0].caracteres.style.color="#ff0000";
+       }else{
+          document.forms[0].caracteres.style.color="#000000";
+       }
+    
+       cuenta()
+    }
+
+    function cuenta(){
+       document.forms[0].caracteres.value=document.forms[0].texto.value.length
+    } 
+
+    function validarFormulario() {
+
+      telefono = document.getElementById("telefono").value;
+      if( !(/^\d{9}$/.test(telefono)) ) {
+      alert("numero no existe");
+      }
+       
+      dni = document.getElementById("dni").value;
+      var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+      
+      if( !(/^\d{8}[A-Z]$/.test(dni)) ) {
+        alert("Error DNI")
+      }
+      
+      if(valor.charAt(8) != letras[(dni.substring(0, 8))%23]) {
+        alert("Error DNI")
+      }
+   
 }
 
 
-function aficiones() {
-    var x = document.getElementById("aficion").value;
-    document.getElementById("texto").innerHTML = "El valor del índice seleccionado es " + x;
-    document.getElementById("texto2").innerHTML = "La longitud de la lista es 3";
-  }
-
-  function myFunction() {
-    document.getElementById("Observaciones").maxLength = "150";
-    }
+   
