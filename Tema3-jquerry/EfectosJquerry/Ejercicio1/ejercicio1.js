befb1e3eaf2dd44843fbf257304c18039bb3a645
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2023-01-24 09:39:03 +0100

    E1 acabado

--- Tema3-jquerry/EfectosJquerry/Ejercicio1/ejercicio1.js
+++ Tema3-jquerry/EfectosJquerry/Ejercicio1/ejercicio1.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-    $("#ejemplo_fade").hover(function(){
-        $(this).find("img").fadeOut();
-    }, function() {
-        $(this).find("img").fadeIn();
-    });
+ 
+    $("#ocultar").click(function(e){				
+        $("#imagen").fadeOut(2000);		
+      });
+      $("#mostrar").click(function(e){				
+        $("#imagen").fadeIn(2000);		
+      });
+      $("#animate").click(function() {
+        $("#imagen").animate({
+        height: "20px",
+        width: "10px",
+        }, 1000);
+        $("#imagen").animate({
+        height: "50px",
+        width: "200px"
+        }, 1000);
+        });
+
+    
+        $("#hide").click(function(e){				
+            $("#imagen").hide();		
+          });
 });
