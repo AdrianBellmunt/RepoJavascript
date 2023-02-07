ec92e5966eb51d6cb05656bde556d5b1aac6fbb0
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2023-02-07 12:00:22 +0100

    E6 corregido

--- Tema3-jquerry/Plugins_Widgets/Ejercicio6/javascript.js
+++ Tema3-jquerry/Plugins_Widgets/Ejercicio6/javascript.js
@@ -2,7 +2,34 @@ $(document).ready(function(){
     $("#accordion").accordion();
   });
   
+  $(document).ready(function() {
+    $("#btn").click((function () {
+        $("#clock").desaparece();
+        
+    }));
+
+    $("#btn2").click((function () {
+        $("#clock").mostrar();
+        
+    }));
+});
+
+
+  jQuery.fn.desaparece = function() {
+    this.each(function(){
+       elem = $(this);
+       elem.css("display", "none");
+    });   
+    return this;
+ }; 
  
+ jQuery.fn.mostrar = function() {
+    this.each(function(){
+       elem = $(this);
+       elem.css("display", "block");
+    });   
+    return this;
+ }; 
 
   setInterval(function(){
     var currentTime = new Date();
